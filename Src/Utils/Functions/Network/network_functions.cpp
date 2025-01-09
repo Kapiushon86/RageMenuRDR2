f400bcd78dc28bb1ac7366498a6c0bcc5d4000f8
Kapiushon <kapiushon86@gmail.com>
2025-01-09 04:51:27 -0500

    Delete Src/Utils/Functions/Network directory

--- Src/Utils/Functions/Network/network_functions.cpp
+++
@@ -1,4 +0,0 @@
-#include "network_functions.h"
-#include "../../../Menu/Base/Submenus/Main/Network/network.h" 
-#include "../../Saving/States/Network/network_default_states.h" 
-
--- Src/Utils/Functions/Network/players_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYERS_FUNCTIONS_H
-#define PLAYERS_FUNCTIONS_H
-
-#include "../../../Menu/Base/Submenus/Main/Network/players.h"
-
-// Update the declaration to accept Submenu* sub
-void Players_NetworkPlayersStatusFunction(Submenu* sub);
-
-#endif
